5b4345a22427c760ef054eed6e4f2bb4c3b36daf
drimlon <107763811+drimlon@users.noreply.github.com>
2025-02-03 02:52:43 +0100

    Update Database_Manager.php

--- includes/Database/Database_Manager.php
+++ includes/Database/Database_Manager.php
@@ -6,10 +6,10 @@
  * @copyright 2024 WPS
  */
 
-namespace WPS\WP\CookieConsent\Database;
+namespace Wps\WP\CookieConsent\Database;
 
-use WPS\WP\CookieConsent\Options\Options;
-use WPS\WP\CookieConsent\Logging\Logger;
+use Wps\WP\CookieConsent\Options\Options;
+use Wps\WP\CookieConsent\Logging\Logger;
 
 use Exception;
 
